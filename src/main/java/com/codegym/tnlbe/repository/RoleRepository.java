2390089f20b8f9e27b5810c4d808ae593c96847f
dohuunghia11 <huunghiado1995@gmail.com>
2019-09-10 09:11:44 +0700

    Edit RoleRepository

--- src/main/java/com/codegym/tnlbe/repository/RoleRepository.java
+++ src/main/java/com/codegym/tnlbe/repository/RoleRepository.java
@@ -5,9 +5,7 @@ import com.codegym.tnlbe.entity.RoleName;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RoleRepository extends CrudRepository<Role, Long> {
-    Optional<Role> findByName(RoleName roleName);
+    Role findByName(RoleName roleName);
 }
